7bda988b0d3a4274e6ad350ebca3a358d0fc2084
shastrikapatel <shastrikapatel24@gmail.com>
2026-07-12 02:13:49 +0530

    new

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ app.get("/", (req, res) => {
 
 app.use("/api/blogs", require("./routes/blogRoutes"));
 
+console.log({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing",
+});
+
 module.exports = app;
 
 if (process.env.NODE_ENV !== "production") {
